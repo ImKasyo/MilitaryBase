3662b3bd4f2b513e4c80dc2107bf6bd1ecf23286
Mandi_pv66 <Mandi_pv66@student.itstep.org>
2024-10-20 15:53:11 +0300

    Add project files.

---
+++ MilitaryBase/Base.cpp
@@ -0,0 +1,5 @@
+#include "Base.h"
+int Base::people_on_base = 0;
+int Base::vehicles_on_base = 0;
+double Base::goods_on_base = 0;
+double Base::pertol_on_base = 0;
---
+++ MilitaryBase/Base.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Base
+{
+	static int people_on_base;
+	static int vehicles_on_base;
+	static double pertol_on_base;
+	static double goods_on_base;
+};
+
